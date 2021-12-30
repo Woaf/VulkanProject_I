dc4a3235d45ca4a28795111bcb94862be5499c30
Woaf <bfazekas@inf.elte.hu>
2021-12-30 09:33:50 +0100

    Initial commit - test code for vulkan api on mac

--- main.cpp
+++ main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
 
-int main () {
-	std::cout << "Hello, World!" << std::endl;
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm.hpp>
+#include <glm/mat4x4.hpp>
+
+int main ()
+{
+	glfwInit ();
+	glfwWindowHint (GLFW_CLIENT_API, GLFW_NO_API);
+	GLFWwindow* mainWindow = glfwCreateWindow (1280, 720, "Test window", nullptr, nullptr);
+
+	uint32_t extensionCount = 0;
+	vkEnumerateInstanceExtensionProperties (nullptr, &extensionCount, nullptr);
+
+	std::cout << "Extension count: " << extensionCount << std::endl;
+
+	glm::mat4 matrix (1.0f);
+	glm::vec4 vector (1.0f);
+	auto testresult = matrix * vector;
+
+
+	while (!glfwWindowShouldClose (mainWindow)) {
+		glfwPollEvents ();
+		glfwSwapBuffers (mainWindow);
+	}
+
+	glfwDestroyWindow (mainWindow);
+	glfwTerminate ();
+
 	return 0;
 }
